95d4a215a8451c6426a698926e5bcbd483868eb9
Prk <prk@prk123.com>
2023-05-25 14:45:43 +0800

    update

--- request.js
+++ request.js
@@ -3,4 +3,42 @@
  * @author Prk
  */
 
-export default uni.request();
+import {
+    baseURL
+} from '@/env';
+
+export default (opt) => {
+    if (opt.loading) uni.showLoading({
+        title: '加载中',
+        mask: true
+    });
+    const {
+        uri,
+        method,
+        data,
+        header
+    } = opt;
+    let apiToken = uni.getStorageSync('api_token');
+    return new Promise((resolve, reject) => {
+        uni.request({
+            url: baseURL + uri,
+            method,
+            data,
+            header: {
+                Authorization: apiToken ? `Bearer ${apiToken}` : undefined,
+                ...header
+            },
+            success: res => {
+                // Request interceptor
+                resolve(res.data);
+            },
+            fail: err => {
+                // Response interceptor
+                reject(err);
+            },
+            complete: () => {
+                if (opt.loading) uni.hideLoading();
+            }
+        });
+    })
+}
