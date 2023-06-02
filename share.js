d4b77ce16e4e1a7bf6a4aa9ce40fb7089ed0b456
Prk <prk@prk123.com>
2023-06-02 12:44:44 +0800

    Weixin(WeChat) Miniprogram Share (Mixin)

---
+++ share.js
@@ -0,0 +1,58 @@
+/**
+ * Weixin(WeChat) Miniprogram Share (Mixin)
+ * @author Prk
+ */
+
+import {
+    APP_NAME
+} from '@/env';
+
+export default {
+    onLoad: function() {
+        wx.showShareMenu({
+            withShareTicket: true,
+            menus: [
+                'shareAppMessage',
+                'shareTimeline'
+            ]
+        });
+    },
+    onShareAppMessage(res) {
+        // res: {from: 'menu', target: undefined}
+        let that = this,
+            route = `/${that.$scope.route}`,
+            opt = that.$scope.options,
+            optStr = that.objToQuery(opt);
+        return {
+            title: APP_NAME,
+            path: `${route}${'' !== optStr ? `?${optStr}` : ''}`,
+            imageUrl: ''
+        }
+    },
+    onShareTimeline() {
+        let that = this,
+            route = `/${that.$scope.route}`,
+            opt = that.$scope.options,
+            optStr = that.objToQuery(opt);
+        return {
+            title: APP_NAME,
+            path: `${route}${'' !== optStr ? `?${optStr}` : ''}`,
+            imageUrl: ''
+        }
+    },
+    methods: {
+        objToQuery(obj) {
+            if (0 === Object.keys(opt).length) return '';
+            let result = [],
+                value = '';
+            for (var key in obj) {
+                value = data[key];
+                if (Array == value.constructor) value.forEach(e => {
+                    result.push(`${key}=${value}`);
+                });
+                else result.push(`${key}=${value}`);
+            }
+            return result.join('&');
+        }
+    }
+}
